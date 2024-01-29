c081de62ae650256a10445ca5212858be853cd03
Eric <126840543+eccol@users.noreply.github.com>
2024-01-28 20:32:04 -0500

    Create Player

---
+++ src/Player.js
@@ -0,0 +1,35 @@
+export class Player {
+  constructor(args) {
+    this.enemyBoard = args.enemyBoard;
+    this.board = args.board;
+    this.guesses = [];
+  }
+
+  attack(coords) {
+    this.enemyBoard.receiveAttack(coords);
+  }
+
+  isWinner() {
+    return this.enemyBoard.areAllSunk();
+  }
+}
+
+export class CPUPlayer extends Player {
+  getMove() {
+    const xMax = this.enemyBoard.xLength;
+    const yMax = this.enemyBoard.yLength;
+    let randomX;
+    let randomY;
+    let moveFound = false;
+
+    while (!moveFound) {
+      randomX = Math.floor(Math.random() * xMax);
+      randomY = Math.floor(Math.random() * yMax);
+      if (!this.enemyBoard.wasAlreadyGuessed([randomX, randomY]))
+        moveFound = true;
+    }
+
+    this.guesses.push([randomX, randomY]);
+    return [randomX, randomY];
+  }
+}
---
+++ test/Player.test.js
@@ -0,0 +1,54 @@
+import { Player, CPUPlayer } from '../src/Player.js';
+import Ship from '../src/Ship.js';
+import Gameboard from '../src/Gameboard.js';
+
+describe('getMove', () => {
+  it('generates valid attack as CPU', () => {
+    const board = new Gameboard(10, 10);
+    const cpu = new CPUPlayer({ enemyBoard: board });
+    const move = cpu.getMove();
+    const fn = () => board.receiveAttack(move);
+    expect(fn).not.toThrow();
+  });
+
+  it('does not duplicate guesses as CPU', () => {
+    const board = new Gameboard(1, 2);
+    const cpu = new CPUPlayer({ enemyBoard: board });
+    const fn = () => board.receiveAttack(cpu.getMove());
+    fn();
+    expect(fn).not.toThrow();
+  });
+});
+
+describe('attack', () => {
+  it('sends an attack to the enemy', () => {
+    const spy = jest.spyOn(Gameboard.prototype, 'receiveAttack');
+    const board = new Gameboard(10, 10);
+    const player = new Player({ enemyBoard: board });
+    player.attack([1, 1]);
+
+    expect(spy).toHaveBeenCalledWith([1, 1]);
+    spy.mockClear();
+  });
+});
+
+describe('isWinner', () => {
+  let board, player;
+
+  beforeEach(() => {
+    board = new Gameboard(10, 10);
+    board.placeShip(new Ship({ length: 2 }), [1, 1]);
+    player = new Player({ enemyBoard: board });
+  });
+
+  it('is true when enemy ships are all sunk', () => {
+    board.receiveAttack([1, 1]);
+    board.receiveAttack([2, 1]);
+    expect(player.isWinner()).toBe(true);
+  });
+
+  it('is false when enemy ships are not sunk', () => {
+    board.receiveAttack([1, 1]);
+    expect(player.isWinner()).toBe(false);
+  });
+});
